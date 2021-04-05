8efd47c2bd900e6821efd70a5c7df45d33cd069c
Maxim Denisov <maxim.denisov@kpbs.ru>
2021-04-05 20:58:18 +0200

    init commit

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
+from config import token
 
 
 def write_msg(chat_id, message):
     vk.method('messages.send', {'chat_id': chat_id, 'message': message, 'random_id': get_random_id()})
 
-
-# API-ключ созданный ранее
-token = "7d3585b1a64101223ccb263ab72adf686b5422e61d0be1650f15aa8e0655fde8e7c23670d5dc757ee88c6"
 
 # Авторизуемся как сообщество
 vk = vk_api.VkApi(token=token)
